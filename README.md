9216db2d97320ca8c5a1f326c1ddc4991f2f9f65
rafael2007br <151068185+rafael2007br@users.noreply.github.com>
2025-04-15 13:29:56 -0300

    salvar

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # projeto-3A
+# Site acessível sobre Tropicália
+## Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS
+# Site acessível sobre Tropicália
+## Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS.
+## Recursos de acessibilidade
+- Atributos aria
+- Alt
+- Tab-index
+- Menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollReveal.js
+- HTML
+- CSS
+- JS
+# Site acessível sobre Tropicália
+## Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS.
+## Recursos de acessibilidade
+- Atributos aria
+- Alt
+- Tab-index
+- Menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollReveal.js
+- HTML
+- CSS
+- JS
---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+//Código omitido
+
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+
+//Código omitido
+//Código omitido
+
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+//Código omitido
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
