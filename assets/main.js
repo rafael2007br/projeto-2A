a8ef384c4a8ab379e9b49395eb88da470e1f686e
rafael2007br <151068185+rafael2007br@users.noreply.github.com>
2025-05-06 13:46:16 -0300

    aleraçoes

---
+++ assets/main.js
@@ -0,0 +1,143 @@
+</section>
+<script src="main.js"></script>
+</body>
+</html>
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+    const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}]
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
+}
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
+.ativo {
+    background-color: var(--botao-ativo);
+    border-bottom: 4px solid var(--verde);
+  }
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+for (let i = 0; i < botoes.length; i++) {
+    console.log(i);
+  }
+  botoes[i].onclick = function () {};
+  botoes[i].onclick = function () {
+    botoes[i].classList.add("ativo");
+  };
+  for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+      botoes[i].classList.add("ativo");
+    };
+  }
+  for (let j = 0; j < botoes.length; j++) {
+    botoes[j].classList.remove("ativo");
+  }
+  const botoes = document.querySelectorAll(".botao");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+  };
+}
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador");
+
+//Código omitido
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem regressiva";
+//Código omitido
+
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+
+contadores[0].textContent = tempo0bjetivo1; 
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+      for (let j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
+      }
+  
+      botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
+    };
+  }
+  contadores[0].textContent = calculaTempo(tempoObjetivo1);
